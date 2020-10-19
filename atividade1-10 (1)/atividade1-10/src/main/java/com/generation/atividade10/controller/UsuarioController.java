b3b1e4149c786f52e6c51c830ad2e86ac16abe69
Alex Meschnark <alex.meschnark@hotmail.com>
2020-10-19 19:18:55 -0300

    Swagger and Deploy

---
+++ atividade1-10 (1)/atividade1-10/src/main/java/com/generation/atividade10/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.generation.atividade10.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.atividade10.model.UserLogin;
+import com.generation.atividade10.model.Usuario;
+import com.generation.atividade10.repository.UsuarioRepository;
+import com.generation.atividade10.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+}
---
+++ atividade1-10 (1)/atividade1-10/src/main/java/com/generation/atividade10/service/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.generation.atividade10.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.generation.atividade10.model.UserLogin;
+import com.generation.atividade10.model.Usuario;
+import com.generation.atividade10.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	@Autowired
+	//Injetando UsuarioRepository através do objeto repository.
+	private UsuarioRepository repository;
+	
+	public Usuario CadastrarUsuario(Usuario usuario)
+	{
+		//Instancia de um novo objeto do tipo BCryptPasswordEncoder chamado encoder.
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		//Senha codificada e armazenada.
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		//Senha salva no banco de dados através do repository.
+		return repository.save(usuario);
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+
+		if (usuario.isPresent()) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+
+				user.get().setToken(authHeader);				
+				user.get().setNome(usuario.get().getNome());
+
+				return user;
+
+			}
+		}
+		return null;
+	}
+}
